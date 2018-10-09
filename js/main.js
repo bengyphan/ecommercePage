2cc59c03a90e5bef11677c9b3eeb2f02450002e0
Bengy Phan <bengyphan@gmail.com>
2018-10-09 16:28:17 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,71 @@
+var shoes = [
+	{
+		name: "Jordan 1"
+		colors: 6,
+		price: "$120",
+		img: "https://www.flightclub.com/media/catalog/product/cache/1/image/1600x1140/9df78eab33525d08d6e5fb8d27136e95/6/3/63611742962-air-jordan-1-retro-high-og-bred-black-varsity-red-white-011845_1.jpg"
+	},
+	{
+		name: "Jordan 2"
+		colors: 3,
+		price: "$140",
+		img: "https://i.ebayimg.com/images/g/Xr4AAOSwstxVfdK2/s-l640.jpg"
+	}, 
+	{
+		name: "Jordan 3"
+		colors: 7,
+		price: "$160",
+		img: "https://www.flightclub.com/media/catalog/product/cache/1/image/1600x1140/9df78eab33525d08d6e5fb8d27136e95/8/0/803847_01.jpg"
+	}, 
+	{
+		name: "Jordan 4"
+		colors: 5,
+		price: "$160",
+		img: ""
+	}, 
+	{
+		name: "Jordan 5"
+		colors: 6,
+		price: "$160",
+	}, 
+	{
+		name: "Jordan 6"
+		colors: 4,
+		price: "$180",
+	}, 
+	{
+		name: "Jordan 7"
+		colors: 4,
+		price: "$180",
+	}, 
+	{
+		name: "Jordan 8"
+		colors: 3,
+		price: "$160",
+	}, 
+	{
+		name: "Jordan 9"
+		colors: 4,
+		price: "$180",
+	}, 
+	{
+		name: "Jordan 10"
+		colors: 10,
+		price: "$160", 
+	}, 
+	{
+		name: "Jordan 11"
+		colors: 6,
+		price: "$200",
+	}, 
+	{
+		name: "Jordan 12"
+		colors: 5,
+		price: "$180",
+	}, 
+	{
+		name: "Jordan 13"
+		colors: 4,
+		price: "$180",
+	},  
+	]
